ab1acacc4d9c52640666b3ae189228704646239c
XFunshine <156063831+XFunshine@users.noreply.github.com>
2024-01-12 13:59:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -22,7 +22,7 @@ async def start_message(c,m):
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
     await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[CodeNova](https://t.me/CodeNovaSec)**",
+        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴢᴇʀᴏ ᴛᴡᴏ, ᴀ ᴄᴜᴛᴇ ᴀɪ ᴘᴏᴡᴇʀᴇᴅ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[CodeNova](https://t.me/CodeNovaSec)**",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
