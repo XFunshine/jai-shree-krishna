1e14d1111ccd4d7de96a27aef0f71dedd751ce23
XFunshine <156063831+XFunshine@users.noreply.github.com>
2024-01-27 12:17:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,16 +21,16 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴢᴇʀᴏ ᴛᴡᴏ, ᴀ ᴄᴜᴛᴇ ᴀɪ ᴘᴏᴡᴇʀᴇᴅ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[CodeNova](https://t.me/CodeNovaSec)**",
+    await m.reply_photo(f"https://te.legra.ph/file/6276480abc40e607d2d6e.png",
+        caption="*जय श्री कृष्णा** 👋\n\n**तुम्हारे हर प्रश्न का समाधान है मेरे पास तो पूछो तुम्हें क्या पूछना है|**\n\n **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[funshine](https://t.me/A_r_e_a_5_1)**",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('💝ʀᴇᴀᴅ ᴅᴏᴄs', url='https://t.me/+iWeGtYLAbZ04OGNl')
+                        InlineKeyboardButton('💝ʀᴇᴀᴅ ʙʜᴀɢᴡᴀᴛ ɢɪᴛᴀ', url='https://t.me/jaishreekeshava')
                     ],  
                     [
-                        InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/CodeNovaSec'),
-                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/+sHqrUkK3eRNmMDk1')
+                        InlineKeyboardButton("❣️ ᴡʜᴏ ᴀᴍ ɪ", url='https://t.me/whoisshreekrishna'),
+                    
                     ]
                 ]
             )
