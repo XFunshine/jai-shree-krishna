6696033747190ab88965160c0e6219756721e9d2
XFunshine <156063831+XFunshine@users.noreply.github.com>
2024-02-11 16:54:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/6276480abc40e607d2d6e.png",
+    await m.reply_photo(f"https://te.legra.ph/file/e38fe103b7b209d799de3.png",
         caption="*ʟᴇᴛ's ᴄʀᴀᴄᴋ ɪᴛ** 👋\n\n**ɪ ᴀᴍ ᴀ ɴᴇᴇᴛɢᴘᴛ ᴀɪ ʙᴀsᴇᴅ ᴏɴ ɴᴄᴇʀᴛ. ᴊᴜsᴛ ᴛʏᴘᴇ ᴀ ǫᴜᴇsᴛɪᴏɴ ᴛᴏ ɢᴇᴛ ᴀɴsᴡᴇʀ**\n\n **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[funshine](https://t.me/A_r_e_a_5_1)**",
         reply_markup=InlineKeyboardMarkup(
                 [
