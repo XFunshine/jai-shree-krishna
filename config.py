85d35b19260c8a8a78fb5d278f590049654157c5
XFunshine <156063831+XFunshine@users.noreply.github.com>
2024-04-09 19:46:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 API_ID = int(environ.get("API_ID", "27947566"))
 API_HASH = environ.get("API_HASH", "9f9f5e44b10b65b576395c0a8d2682a0")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6934583129:AAGExQbCnN6uE0V182B2JZylfEaYVcapT-Q")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7179645509:AAFGl1ordN-TycGvx3FHwzYx0wvuXD3vols")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002016103125"))
 ADMINS = int(environ.get("ADMINS", "1715422029"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://nova:novabro99@cluster0.wkhngfn.mongodb.net/?retryWrites=true&w=majority")
