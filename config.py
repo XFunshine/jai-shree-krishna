fd660eb1baa10fb4f2db7fac4b71483d562c292e
XFunshine <156063831+XFunshine@users.noreply.github.com>
2024-05-23 15:53:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 API_ID = int(environ.get("API_ID", "27947566"))
 API_HASH = environ.get("API_HASH", "9f9f5e44b10b65b576395c0a8d2682a0")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7179645509:AAFGl1ordN-TycGvx3FHwzYx0wvuXD3vols")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002016103125"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "6886387315:AAF8LIDg-WZQ3wUD1i9Dt4sIcLFm8l6-3Wk")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002150388284"))
 ADMINS = int(environ.get("ADMINS", "1715422029"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://nova:novabro99@cluster0.wkhngfn.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
